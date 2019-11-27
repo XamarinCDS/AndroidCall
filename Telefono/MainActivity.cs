dd081d77a848e7e4aefa407b6e37a364486b1e08
CDS-SA-01 <cds-sa-01@iMac-de-CDS-SA-01.local>
2019-11-27 08:56:05 -0600

    First commit

---
+++ Telefono/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Telefono
+{
+    [Activity(Label = "PhoneApp", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.MainPage);
+            var PhoneNumberText = FindViewById<EditText>(Resource.Id.txtNumero);
+            var TranslateButton = FindViewById<Button>(Resource.Id.btnConvertir);
+            var CallButton = FindViewById<Button>(Resource.Id.btnLlamar);
+            var TextNumbre = FindViewById<TextView>(Resource.Id.lblConvertido);
+            CallButton.Enabled = false;
+            var TranslatedNumber = string.Empty;
+            TranslateButton.Click += (object sender, System.EventArgs e) => {
+                var Translator = new PhoneTranslator();
+                TranslatedNumber = Translator.ToNumber(PhoneNumberText.Text);
+                if (!string.IsNullOrWhiteSpace(TranslatedNumber))
+                {
+                    CallButton.Enabled = true;
+                    CallButton.Text = "Call " + TranslatedNumber;
+                    TextNumbre.Text = TranslatedNumber;
+                }
+                else
+                {
+                }
+            };
+            CallButton.Click += (object sender, System.EventArgs e) => {
+                // Intentar marcar el número telefónico
+                var CallDialog = new global::Android.App.AlertDialog.Builder(this);
+                CallDialog.SetMessage($"Llamar al número {TranslatedNumber}?");
+                CallDialog.SetNeutralButton("Llamar", delegate
+                {
+                    // Crear un intento para marcar el número telefónico
+                    var CallIntent = new global::Android.Content.Intent(global::Android.Content.Intent.ActionCall);
+                    CallIntent.SetData(global::Android.Net.Uri.Parse($"tel:{TranslatedNumber}"));
+                    StartActivity(CallIntent);
+                });
+                CallDialog.SetNegativeButton("Cancelar", delegate { });
+                // Mostrar el cuadro de diálogo al usuario y esperar una respue CallDialog.Show();
+            };
+
+        }
+    }
+}
+
---
+++ Telefono/PhoneTranslator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace Telefono
+{
+    public class PhoneTranslator
+    {
+        string Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string Numbers = "22233344455566677778889999";
+        public string ToNumber(string alfanumericPhoneNumber)
+        {
+            var NumericPhoneNumber = new StringBuilder();
+            if (!string.IsNullOrWhiteSpace(alfanumericPhoneNumber))
+            {
+                alfanumericPhoneNumber = alfanumericPhoneNumber.ToUpper(); foreach (var c in alfanumericPhoneNumber)
+                {
+                    var Index = Letters.IndexOf(c); if (Index >= 0)
+                    {
+                        // Es una letra, obtener el dígito equivalente
+                        NumericPhoneNumber.Append(Numbers[Index]);
+                    }
+                    else
+                    {
+                        NumericPhoneNumber.Append(c);
+                    }
+                }
+            }
+            return NumericPhoneNumber.ToString();
+        }
+        public PhoneTranslator()
+        {
+        }
+    }
+}
